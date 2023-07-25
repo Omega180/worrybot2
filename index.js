d365a808b2342954a8a17bb7fb8309ec62ebd2f8
CiroDelgado1803 <cirodelgado1803@gmail.com>
2023-07-25 17:43:36 -0400

    Movido los comandos a cada carpeta y creado el listener que escucha las interacciones recibidas por el bot

--- index.js
+++ index.js
@@ -7,26 +7,59 @@ const {token} = require("./config.json")
 // Crear una nueva instancia del cliente
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]})
+
 client.commands = new Collection()
+const foldersPath = path.join(__dirname, "commands")
+const commandFolders = fs.readdirSync(foldersPath)
 
-const commandsPath = path.join(__dirname, "commands")
-const commandFiles = fs
-	.readdirSync(commandsPath)
-	.filter((file) => file.endsWith(".js"))
-
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file)
-        const command = require(filePath)
-        // coloca un nuevo item en la coleccion con la llave del nombre del comando y el valor del modulo exportado
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command)
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`)
-        }
-    }
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder)
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith(".js"))
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file)
+		const command = require(filePath)
+		// coloca un nuevo item en la coleccion con la llave del nombre del comando y el valor del modulo exportado
+		if ("data" in command && "execute" in command) {
+			client.commands.set(command.data.name, command)
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property`
+			)
+		}
+	}
+}
 
 // Cuando el cliente este listo, correr este codigo (solo una vez)
 // Usamos 'c' como parametro del evento para mantenerlo separado del ya definido "client"
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return
+	const command = interaction.client.commands.get(interaction.commandName)
+
+	if (!command) {
+		console.error(`no command matching ${interaction.commandName} was found`)
+		return
+	}
+
+	try {
+		await command.execute(interaction)
+	} catch (error) {
+		console.error(error)
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({
+				content: "there was an error while executing this command",
+				ephemeral: true,
+			})
+		} else {
+			await interaction.reply({
+				content: "there was an error while executing this command",
+				ephemeral: true,
+			})
+		}
+	}
+})
 
 client.once(Events.ClientReady, (c) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`)
