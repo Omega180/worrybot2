5c5c8b43c230d9b1bfea4501c378eb8efd8adad9
CiroDelgado1803 <cirodelgado1803@gmail.com>
2023-07-27 15:40:01 -0400

    añadido el cooldown al comando ping

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,4 @@
-const {Events} = require("discord.js")
+const {Events, Collection} = require("discord.js")
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -10,12 +10,37 @@ module.exports = {
 			console.error(`no commands matching ${interaction.commandName} was found`)
 			return
 		}
+		// Extraemos la parte de cooldowns desde el cliente con la interaccion realizada
+		const {cooldowns} = interaction.client
+		if (!cooldowns.has(command.data.name)) {
+			cooldowns.set(command.data.name, new Collection())
+		}
+
+		const now = Date.now()
+		const timestamps = cooldowns.get(command.data.name)
+		const defaultCooldownDuration = 3
+		const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000
+
+		if (timestamps.has(interaction.user.id)) {
+			const expirationTime =
+				timestamps.get(interaction.user.id) + cooldownAmount
+			if (now < expirationTime) {
+				const expiredTimestamp = Math.round(expirationTime / 1000)
+				return interaction.reply({
+					content: `Please wait, you are on cooldown for ${command.data.name}. you can use it again in <t:${expiredTimestamp}:R>`,
+					ephemeral: true,
+				})
+			}
+		}
+
+		timestamps.set(interaction.user.id, now)
+		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount)
 
 		try {
 			await command.execute(interaction)
 		} catch (error) {
-			console.error(`Error executing ${interaction.commandName}`)
 			console.error(error)
+			console.error(`Error executing ${interaction.commandName}`)
 		}
 	},
 }
