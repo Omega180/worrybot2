39b3befa5f531fc44a7a376ef0aba7b938700dc2
CiroDelgado1803 <cirodelgado1803@gmail.com>
2023-08-18 16:33:09 -0400

    añadido comando acontecimientos

---
+++ commands/fun/acontecimientos.js
@@ -0,0 +1,25 @@
+const {SlashCommandBuilder} = require("discord.js")
+const wait = require("node:timers/promises").setTimeout
+const listaAcontecimientos = [
+	"El Kraken",
+	"La laptop de Yoshi",
+	"Miedo a la gravedad",
+	"El Wacazo de Neme",
+	"Drinking Sonos (incluyendo parte 2)",
+	"Disco bonito",
+	"El voice chat después de las 2 AM",
+	"Pablo",
+	"La explotación laboral de Howard Jones",
+	"Pablo Expansion Pack 1: GFL ES y Tortuga arc",
+]
+const d6 = () => {
+	return Math.floor(Math.random() * 9)
+}
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("acontecimientos")
+		.setDescription("Muestra un acontecimiento perturbador de poi al azar"),
+	async execute(interaction) {
+		await interaction.reply(`${listaAcontecimientos[d6()]}`)
+	},
+}
